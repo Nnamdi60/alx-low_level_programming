7b4d0acbf0d57ce653b543084f2e7ab832ded33a
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-08 20:21:38 -0700

    Want to put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,27 @@
-1 #include <stdio.h>
-2 /**
-3 
+#include <stdio.h>
 
- *  main - Entry point
+/**
  *
-5 
+ *  *  main - Entry point
+ *
+ *   *
+ *
+ *    *  Description: 'the program's description'
+ *
+ *     *
+ *
+ *      *  Return: Always 0 (Success)
+ *
+ *       */
 
- *  Description: 'the program's description'
- *
-7 *  Return: Always 0 (Success)
-8  */
-9
-10 int main(void)
-11 {
-12   puts("\"Programming is like building a multilingual puzzle");
-13 return (0);
-14 }
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
