3226f0ab399830683796a8d57f77abb74ebef3b7
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-08 20:18:42 -0700

    Want to put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+1 #include <stdio.h>
+2 /**
+3 
+
+ *  main - Entry point
+ *
+5 
+
+ *  Description: 'the program's description'
+ *
+7 *  Return: Always 0 (Success)
+8  */
+9
+10 int main(void)
+11 {
+12   puts("\"Programming is like building a multilingual puzzle");
+13 return (0);
+14 }
