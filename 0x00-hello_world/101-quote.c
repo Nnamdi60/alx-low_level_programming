be9a158d00d6eb12a036e6c693427331315c1a6f
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-09 07:54:43 -0700

    The greate quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 /**
  * main - Entry point
+ * Description: 'the description ot the program'
  *
- * Description: 'the description of the program'
- *
- * Return: will be 1 on success
+ * Return: wii be 1 on success
  */
 
-int main(void )
+int main(void)
 {
-	write(2, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(2, "and that piece of art is useful\" Dora - Korpar, 2015-10-19\n", 59);
+	return (1)
 }
