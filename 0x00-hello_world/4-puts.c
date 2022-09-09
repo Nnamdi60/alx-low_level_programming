fbb97ea61ed18bd43c0b47dea2e5cdf3d991ffdb
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-08 21:01:35 -0700

    the puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
-
 /**
+ *  main - Entry point
  *
- *  *  main - Entry point
- *
- *   *
- *
- *    *  Description: 'the program's description'
- *
- *     *
- *
- *      *  Return: Always 0 (Success)
- *
- *       */
-
-
-
+ *  Description: 'the program's description'
+ *  Return : Always 0 (sucess)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
