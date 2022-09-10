cf8352b20a0cef0809ed55551243a5515398e839
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-09 22:51:40 -0700

    Invest in brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,39 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - print triple combos
- *
- * Description: print triple digit combinations
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j, k;
+	int d;
+	for (d = 0; d < 100; d++)
 
-	i = 48;
-	j = 48;
-	k = 48;
-	while (i < 58)
 	{
-		j = i + 1;
-		while (j < 58)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
 		{
-			putchar(i);
-			putchar(j);
-			putchar(k);
-			if (i < 55 || j < 56 || k < 57)
-			{
-				putchar(44);
-				putchar(32);
-			                	k++;
-		                        	}
-			                        j++;
-	                  			} 
-						i++;
-						}
-						putchar(10);
-						return (0);
-}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
+	return (0);
+}
