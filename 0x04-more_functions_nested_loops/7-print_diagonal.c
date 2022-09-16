48409c48d8cbcb625bcf5ea93bb69144792d1be3
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-15 22:17:45 -0700

    The rea one

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,14 @@
  * Return: Always 0.
  */
 
-
 void print_diagonal(int n)
 
 {
+
 	int i;
 
 	int b;
+
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
@@ -23,8 +24,7 @@ void print_diagonal(int n)
 			}
 			_putchar (92);
 
-			_putchar ('\n')
-
+			_putchar ('\n');
 		}
 	}
 	else
