4f993c7738c1b7bdf8d151187e70aca32d6e98ec
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-19 16:51:52 -0700

    Battery Street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Christopher Christian Nnamdi
+ */
+
+
+#include "main.h"
+
+/**
+ * reset_to_98 - use a pointer to change integer value
+ * @n: parameter
+ * Description - reset_to_98 resets the value of *n to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
