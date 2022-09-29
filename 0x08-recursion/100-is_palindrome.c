2694348e5d96a312cbbdcbc9a09219d8214b1bd2
Nnamdi60 <christiannnamdi60@gmail.com>
2022-09-28 21:03:14 -0700

    Brain true potential

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,54 @@
 #include "main.h"
+
+int _strlen_recursion(char *s);
+int helper_palindrome(char *s, int len);
+
 /**
- * _strlen_recursion - finds the length of a string
- * @s: pointer to string
- * Return: length of string
- **/
+ * _strlen_recursion - A function to obtain the length of the string s
+ * @s: A string to calculate length
+ * Return: the length of string s
+ */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (!*s)
+	{
 		return (0);
+	}
 	return (_strlen_recursion(s + 1) + 1);
 }
+
 /**
- * is_palind_recursive - checks if two chars of a string are equal
- * @s: string to be checked
- * @i: first index
- * @j: last index
- * Return: 1 if equal, else 0
- **/
-int is_palind_recursive(char *s, int i, int j)
-{
-	if (i == j)
-		return (1);
-	if (i == j - 1)
-		return (s[i] == s[j]);
-	if (s[i] != s[j])
-		return (0);
-	return (is_palind_recursive(s, i + 1, j - 1));
-}
-/**
- * is_palindrome - checks if a string is a palindrome
- *  @s: pointer to string
- *  Return: 1 if string is palindrome  else 0
- **/
+ * is_palindrome - A function that checks if s is a palindrome string
+ * @s: An inpuit string
+ * Return: 1 if is string is a palindrome or 0 in otherwise
+ */
 int is_palindrome(char *s)
 {
 	int len;
 
 	len = _strlen_recursion(s);
-	if (len == 0 || *s != s[len - 1])
-		eturn (0);
-	return (is_palind_recursive(s, 0, len - 1));
+	if (len <= 1)
+		return (1);
+	return (helper_palindrome(s, len));
 }
 
+/**
+ * helper_palindrome - A function with a revesed string
+ * @s: An input string
+ * @len: the length of the string s
+ * Return: A reverse string
+ */
+
+int helper_palindrome(char *s, int len)
+{
+
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
+	{
+		return (helper_palindrome(s + 1, len - 2));
+	}
+	else
+		return (0);
+
+}
