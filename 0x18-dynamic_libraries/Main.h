fad4894a1af36f84a0de635e89ecd81bfecc6cfe
Nnamdi60 <christiannnamdi60@gmail.com>
2022-12-12 17:39:23 -0800

    The main deal

--- 0x18-dynamic_libraries/Main.h
+++ 0x18-dynamic_libraries/Main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DYNLIB
+#define DYNLIB
 
-#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -11,7 +10,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -25,3 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+#endif
